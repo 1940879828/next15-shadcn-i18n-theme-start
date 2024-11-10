391b184a9f5156079ae0217bcf5d42e94db383a5
yejiechen <1940879828@qq.com>
2024-11-10 20:31:54 +0800

    feat: 切换夜间模式添加动画

--- src/components/modeToggle.tsx
+++ src/components/modeToggle.tsx
@@ -13,7 +13,56 @@ import {
 import {Moon, Sun} from "lucide-react";
 
 export function ModeToggle() {
-  const { setTheme } = useTheme()
+  const { setTheme, theme } = useTheme()
+  const changeTheme = (e: React.MouseEvent<HTMLDivElement>, value:"light" | "dark" | "system") => {
+    if (theme === value) return
+
+    const transition = document.startViewTransition(async () => {
+      // 如果当前是暗色模式，添加类名以控制 z-index
+      if (theme === "light") {
+        document.documentElement.classList.add('dark-transition');
+      } else {
+        document.documentElement.classList.remove('dark-transition');
+      }
+      setTheme(value)
+    })
+
+    // 在 transition.ready 的 Promise 完成后，执行自定义动画
+    transition.ready.then(() => {
+      // 由于我们要从鼠标点击的位置开始做动画，所以我们需要先获取到鼠标的位置
+      const { clientX, clientY } = e
+
+      // 计算半径，以鼠标点击的位置为圆心，到四个角的距离中最大的那个作为半径
+      const radius = Math.hypot(
+        Math.max(clientX, innerWidth - clientX),
+        Math.max(clientY, innerHeight - clientY)
+      )
+      const clipPath = [
+        `circle(0% at ${clientX}px ${clientY}px)`,
+        `circle(${radius}px at ${clientX}px ${clientY}px)`
+      ]
+      // 自定义动画
+      document.documentElement.animate(
+        {
+          // 如果要切换到暗色主题，我们在过渡的时候从半径 100% 的圆开始，到 0% 的圆结束
+          clipPath: theme === "light" ? clipPath.reverse() : clipPath
+        },
+        {
+          duration: 500,
+          // 如果要切换到暗色主题，我们应该裁剪 view-transition-old(root) 的内容
+          pseudoElement: theme === "light"
+            ? '::view-transition-old(root)'
+            : '::view-transition-new(root)'
+        }
+      )
+      // 确保在动画完成后移除类
+      transition.ready.then(() => {
+        if (theme === "dark") {
+          document.documentElement.classList.remove('dark-transition');
+        }
+      });
+    })
+  }
 
   return (
     <DropdownMenu>
@@ -25,13 +74,13 @@ export function ModeToggle() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
+        <DropdownMenuItem onClick={(e) => changeTheme(e,"light")}>
           Light
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
+        <DropdownMenuItem onClick={(e) => changeTheme(e,"dark")}>
           Dark
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
+        <DropdownMenuItem onClick={(e) => changeTheme(e,"system")}>
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
