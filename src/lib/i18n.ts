6a7de6814e4e92a0fb29ee6bdebadba7c4f4c43f
yejiechen <1940879828@qq.com>
2024-11-10 19:45:06 +0800

    feat: i18n添加缓存

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,5 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 import Chinese from "../../locales/zh.json"
 import English from "../../locales/en.json"
 
@@ -9,14 +10,18 @@ const resources = {
 };
 
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources,
-    lng: "English",
     fallbackLng: "English",
     interpolation: {
       escapeValue: false
-    }
+    },
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage'],
+    },
   });
 
 export default i18n;
